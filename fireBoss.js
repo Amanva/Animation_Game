dc05f1b6b7f6da896b422af89fd770822959015c
Arashpreet123 <51033451+Arashpreet123@users.noreply.github.com>
2023-01-31 15:36:11 -0800

    update fireboss

--- fireBoss.js
+++ fireBoss.js
@@ -12,12 +12,15 @@ class fireBoss{
         this.x = 300;
         this.y = 300;
         this.speed = 100;
-        this.facing = 0; //0=right, 1 = left
-        this.state = 1; // 0 = idle, 1 = walking , 2 = attacking, 3 = hit, 4 = death, 5 = spawn, 6 = jump, 7 = fire attack, 8 = magic attack
-        
+        this.facing = 0; //0=left, 1 = right
+        this.state = 3; // 0 = idle, 1 = walking , 2 = attacking, 3 = hit, 4 = death, 5 = spawn, 6 = jump, 7 = fire attack, 8 = magic attack
+        this.dead = false;
+        this.health = 300;
         //animations
         this.animations = [];
         this.loadAnimations();
+        this.updateBB();
+
     };
 
     loadAnimations() {
@@ -28,61 +31,61 @@ class fireBoss{
             }
         }
 
-        // 
-        // right idle
+        //left 
+        // idle
         this.animations[0][0] = new Animator(this.spritesheet, 103, 50, 100, 108, 6, .2 ,188, 0, false, true, false);
 
-        // right walking
+        // walking
         this.animations[1][0] = new Animator(this.spritesheet, 103, 210, 100, 108, 12, .2 ,188, 0, false, true, false);
 
-        //right attacking
+        // attacking
         this.animations[2][0] = new Animator(this.spritesheet, 10, 367, 200, 115, 15, .2 ,88, 0, false, true, false);
 
-        //right hit
+        // hit
         this.animations[3][0] = new Animator(this.spritesheet, 10, 531, 200, 115, 5, .2 ,88, 0, false, true, false);
 
-        //right death
+        // death
         this.animations[4][0] = new Animator(this.spritesheet, 50, 700, 200, 115, 22, .2 ,88, 0, false, true, false);
 
-        //right spawn 
+        // spawn 
         this.animations[5][0] = new Animator(this.spritesheetSpecialAndSlime,85, 790, 125, 110, 19, .2, 163, 0, false,true, false)
 
-        //right jump
+        // jump
         this.animations[6][0] = new Animator(this.spritesheetSpecialAndSlime,50, 925, 200, 135, 18, .2, 88, 0, false,true, false)
 
-        //right fire attack
+        // fire attack
         this.animations[7][0] = new Animator(this.spritesheetSpecialAndSlime,50,1110,200, 110,21, .2, 88, 0, false,true, false)
 
-        //right magic attack
+        // magic attack
         this.animations[8][0] = new Animator(this.spritesheetSpecialAndSlime,0,1255,215, 125,18, .2, 73, 0, false,true, false)
 
 
-        
-        //left idle
+        //right
+        // idle
         this.animations[0][1] = new Animator(this.spritesheetLeft, 4705, 50, 100, 108, 6, .2 , 188, 0, true, true, false);
 
-        //left walking
+        // walking
         this.animations[1][1] = new Animator(this.spritesheetLeft, 2970, 210, 100, 108, 12, .2 ,188, 0, true, true, false);
 
-        //left attacking
+        // attacking
         this.animations[2][1] = new Animator(this.spritesheetLeft, 2090, 367, 200, 115, 15, .2 ,88, 0, true, true, false);
 
-        //left hit
+        // hit
         this.animations[3][1] = new Animator(this.spritesheetLeft, 4960, 531, 200, 115, 5, .2 ,88, 0, false, true, false);
 
-        //left death
+        // death
         this.animations[4][1] = new Animator(this.spritesheetLeft, 0, 700, 200, 115, 22, .2 ,88, 0, true, true, false);
 
-        //left spawn 
+        // spawn 
         this.animations[5][1] = new Animator(this.spritesheetSpecialAndSlimeLeft,940,795 , 125, 110, 19, .2, 163, 0, true,true, false)
 
-        //left jump
+        // jump
         this.animations[6][1] = new Animator(this.spritesheetSpecialAndSlimeLeft,1190, 925, 210, 135, 18, .2, 78, 0, true,true, false)
     
-        //left fire attack
+        // fire attack
         this.animations[7][1] = new Animator(this.spritesheetSpecialAndSlimeLeft,362,1060,210, 160,21, .2, 78, 0, true,true, false)
 
-        //left magic attack
+        // magic attack
         this.animations[8][1] = new Animator(this.spritesheetSpecialAndSlimeLeft,1230,1250,215, 130,18, .2, 73, 0, true ,true, false)
     }
 
@@ -93,10 +96,61 @@ class fireBoss{
         // }
 
     };
+    updateBB() {
+        this.lastBB = this.BB;
+        this.lastAttackBB = this.AttackBB;
+        if(this.state === 0){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+            // this.AttackBB = new BoundingBox(this.x+50, this.y+100, 0,0);
 
+        }
+        else if(this.state === 2){ // attack
+            this.BB = new BoundingBox(this.x+50, this.y+100, 550, 235);
+            this.AttackBB = new BoundingBox(this.x+50, this.y+100, 200,235);
+        }
+        else if(this.state === 3){
+            this.BB = new BoundingBox(this.x+290, this.y+100, 300, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+        else if(this.state === 1){
+            this.BB = new BoundingBox(this.x, this.y+100, 250, 225);
+        }
+
+
+    };
+
+    updateBBAttack(){
+
+    }
 
     draw(ctx) {
         this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x, this.y, PARAMS.SCALE);
+        this.animations[5][this.facing].drawFrame(this.game.clockTick, ctx, this.x, this.y, PARAMS.SCALE);
+
+            ctx.strokeStyle = 'Red';
+            ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
+            ctx.strokeStyle = 'Green';
+            // ctx.strokeRect(this.AttackBB.x, this.AttackBB.y, this.AttackBB.width, this.AttackBB.height);
 
     };
 
