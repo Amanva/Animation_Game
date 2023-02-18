816fe2fdb2706da865e9604dde91669b67921400
uhnv <58312711+uhnv@users.noreply.github.com>
2023-02-18 14:00:59 -0800

    General load level method

--- sceneManager.js
+++ sceneManager.js
@@ -9,7 +9,7 @@ class SceneManager {
         this.level = null;
         this.myCursor = new Cursor(this.game);
         
-        this.loadLevelOne(levelOne);
+        this.loadLevel(levelOne);
         
     };
 
@@ -19,17 +19,110 @@ class SceneManager {
         });
     }
 
-    loadLevelOne(level){
+    loadLevel(level){
         this.level = level;
         this.clearEntities();
+
+        if (level === levelOne) {
+            this.mage = new Mage(this.game, 662, 488);
+            this.game.addEntity(this.mage);
+            // this.mage = new Mage(this.game, 9300, 400);
             
-        this.mage = new Mage(this.game, 662, 488);
+            this.heartMana = new HeartManaHQ(this.game, this.mage);
+            this.fireBoss = new fireBoss(this.game, 9600, 300); 
+            this.game.addEntity(this.fireBoss);
+
+            this.game.addEntity(this.mage);
+            // this.enemy = new ChainBot(this.game, 50, 500); 
+            // this.game.addEntity(this.enemy);
+            // this.enemy = new ChainBot(this.game, 350, 300); 
+            // this.game.addEntity(this.enemy);
+            // this.enemy = new ChainBot(this.game, 550, 300); 
+            // this.game.addEntity(this.enemy);
+
+            // this.monster = new Monster(this.game, 650, 250); 
+            // this.game.addEntity(this.monster);
+            this.portal = new Portal(this.game, 200, 430);
+            this.game.addEntity(this.portal);
+
+            if(level.ChainBot){
+                for (var i = 0; i < level.ChainBot.length; i++) {
+                    let chainBot = level.ChainBot[i];
+                    this.game.addEntity(new ChainBot(this.game, chainBot.x, chainBot.y));
+                }
+            }
+
+            if(level.Monster){
+                for (var i = 0; i < level.Monster.length; i++) {
+                    let monster = level.Monster[i];
+                    this.game.addEntity(new Monster(this.game, monster.x, monster.y));
+                }
+            }
+
+            if(level.ground){
+                for (var i = 0; i < level.ground.length; i++) {
+                    let ground = level.ground[i];
+                    this.game.addEntity(new Ground(this.game, ground.x, ground.y, ground.width, ground.height, ground.div));
+                }
+            }
+            if(level.platforms){
+                for (var i = 0; i < level.platforms.length; i++) {
+                    let platform = level.platforms[i];
+                    this.game.addEntity(new Platform(this.game, platform.x, platform.y, platform.width, platform.height, platform.divisorPlatforms));
+                }
+            }
+            if(level.movingPlatforms){
+                for (var i = 0; i < level.movingPlatforms.length; i++) {
+                    let wall = level.movingPlatforms[i];
+                    this.game.addEntity(new movingPlatforms(this.game, wall.x, wall.y, wall.width, wall.height, wall.divisorPlatforms, wall.direction, wall.distance));
+                }
+            }
+            if(level.verticalWall){
+                for (var i = 0; i < level.verticalWall.length; i++) {
+                    let tiles = level.verticalWall[i];
+                    this.game.addEntity(new verticalWall(this.game, tiles.x, tiles.y, tiles.width, tiles.height, tiles.div));
+                }
+            }
+            if(level.smallPlatforms){
+                for (var i = 0; i < level.smallPlatforms.length; i++) {
+                    let tiles = level.smallPlatforms[i];
+                    this.game.addEntity(new smallPlatforms(this.game, tiles.x, tiles.y, tiles.width, tiles.height, tiles.div));
+                }
+            }
+            if(level.wall){
+                for (var i = 0; i < level.wall.length; i++) {
+                    let wall = level.wall[i];
+                    this.game.addEntity(new Wall(this.game, wall.x, wall.y, wall.width, wall.height, wall.div));
+                }
+            }
+            if(level.tiles){
+                for (var i = 0; i < level.tiles.length; i++) {
+                    let tiles = level.tiles[i];
+                    this.game.addEntity(new Tiles(this.game, tiles.x, tiles.y, tiles.width, tiles.height, tiles.div));
+                }
+            }
+            if(level.gate){
+                for (var i = 0; i < level.gate.length; i++) {
+                    let gate = level.gate[i];
+                    this.game.addEntity(new Gate(this.game, gate.x, gate.y, gate.wallX, gate.wallY, gate.wallWidth, gate.wallHeight, gate.div));
+                }
+
+            }
+            
+            this.game.addEntity(new LevelOneBackGround(this.game, 0, 0, 1800, 800));        
+            // this.mage = new Mage(this.game, 662, 488);
+            // this.game.addEntity(this.mage);
+            
+            // this.mage.velocity = { x: 0, y: 0 };
+
+        } else if( level === levelTwo) {
+            this.mage = new Mage(this.game, 662, 488);
         this.game.addEntity(this.mage);
         // this.mage = new Mage(this.game, 9300, 400);
         
         this.heartMana = new HeartManaHQ(this.game, this.mage);
-        this.fireBoss = new fireBoss(this.game, 9600, 300); 
-        this.game.addEntity(this.fireBoss);
+        // this.fireBoss = new fireBoss(this.game, 9600, 300); 
+        // this.game.addEntity(this.fireBoss);
 
         this.game.addEntity(this.mage);
         // this.enemy = new ChainBot(this.game, 50, 500); 
@@ -41,106 +134,10 @@ class SceneManager {
 
         // this.monster = new Monster(this.game, 650, 250); 
         // this.game.addEntity(this.monster);
-        this.portal = new Portal(this.game, 200, 430);
-        this.game.addEntity(this.portal);
+        // this.cave = new Cave(this.game, 200, 430);
+        // this.game.addEntity(this.cave);
 
-        if(level.ChainBot){
-            for (var i = 0; i < level.ChainBot.length; i++) {
-                let chainBot = level.ChainBot[i];
-                this.game.addEntity(new ChainBot(this.game, chainBot.x, chainBot.y));
-            }
-        }
-
-        if(level.Monster){
-            for (var i = 0; i < level.Monster.length; i++) {
-                let monster = level.Monster[i];
-                this.game.addEntity(new Monster(this.game, monster.x, monster.y));
-            }
-        }
-
-        if(level.ground){
-            for (var i = 0; i < level.ground.length; i++) {
-                let ground = level.ground[i];
-                this.game.addEntity(new Ground(this.game, ground.x, ground.y, ground.width, ground.height, ground.div));
-            }
-        }
-        if(level.platforms){
-            for (var i = 0; i < level.platforms.length; i++) {
-                let platform = level.platforms[i];
-                this.game.addEntity(new Platform(this.game, platform.x, platform.y, platform.width, platform.height, platform.divisorPlatforms));
-            }
-        }
-        if(level.movingPlatforms){
-            for (var i = 0; i < level.movingPlatforms.length; i++) {
-                let wall = level.movingPlatforms[i];
-                this.game.addEntity(new movingPlatforms(this.game, wall.x, wall.y, wall.width, wall.height, wall.divisorPlatforms, wall.direction, wall.distance));
-            }
-        }
-        if(level.verticalWall){
-            for (var i = 0; i < level.verticalWall.length; i++) {
-                let tiles = level.verticalWall[i];
-                this.game.addEntity(new verticalWall(this.game, tiles.x, tiles.y, tiles.width, tiles.height, tiles.div));
-            }
-        }
-        if(level.smallPlatforms){
-            for (var i = 0; i < level.smallPlatforms.length; i++) {
-                let tiles = level.smallPlatforms[i];
-                this.game.addEntity(new smallPlatforms(this.game, tiles.x, tiles.y, tiles.width, tiles.height, tiles.div));
-            }
-        }
-        if(level.wall){
-            for (var i = 0; i < level.wall.length; i++) {
-                let wall = level.wall[i];
-                this.game.addEntity(new Wall(this.game, wall.x, wall.y, wall.width, wall.height, wall.div));
-            }
-        }
-        if(level.tiles){
-            for (var i = 0; i < level.tiles.length; i++) {
-                let tiles = level.tiles[i];
-                this.game.addEntity(new Tiles(this.game, tiles.x, tiles.y, tiles.width, tiles.height, tiles.div));
-            }
-        }
-        if(level.gate){
-            for (var i = 0; i < level.gate.length; i++) {
-                let gate = level.gate[i];
-                this.game.addEntity(new Gate(this.game, gate.x, gate.y, gate.wallX, gate.wallY, gate.wallWidth, gate.wallHeight, gate.div));
-            }
-
-        }
-        
-        this.game.addEntity(new LevelOneBackGround(this.game, 0, 0, 1800, 800));        
-        // this.mage = new Mage(this.game, 662, 488);
-        // this.game.addEntity(this.mage);
-        
-        // this.mage.velocity = { x: 0, y: 0 };
-        
-    }
-
-    loadLevelTwo(level){
-        this.level = level;
-        this.clearEntities();
-         
-        this.mage = new Mage(this.game, 662, 488);
-        this.game.addEntity(this.mage);
-        // this.mage = new Mage(this.game, 9300, 400);
-        
-        this.heartMana = new HeartManaHQ(this.game, this.mage);
-        this.fireBoss = new fireBoss(this.game, 9600, 300); 
-        this.game.addEntity(this.fireBoss);
-
-        this.game.addEntity(this.mage);
-        // this.enemy = new ChainBot(this.game, 50, 500); 
-        // this.game.addEntity(this.enemy);
-        // this.enemy = new ChainBot(this.game, 350, 300); 
-        // this.game.addEntity(this.enemy);
-        // this.enemy = new ChainBot(this.game, 550, 300); 
-        // this.game.addEntity(this.enemy);
-
-        // this.monster = new Monster(this.game, 650, 250); 
-        // this.game.addEntity(this.monster);
-        this.portal = new Portal(this.game, 200, 430);
-        this.game.addEntity(this.portal);
-
+//Level = levelTwo, the take assets fromthe levelTwo
         if(level.ChainBot){
             for (var i = 0; i < level.ChainBot.length; i++) {
                 let chainBot = level.ChainBot[i];
@@ -204,7 +201,13 @@ class SceneManager {
         // this.game.addEntity(this.mage);
         
         // this.mage.velocity = { x: 0, y: 0 };
+            
+        }
+        
+        
     }
+
+    
 
     
     update() {
